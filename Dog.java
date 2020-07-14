307224418d0d9fbe270f65a215865d5241c994bc
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-07-14 12:24:40 -0600

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,23 @@
+// 1. Write your class
+class dog {
+  // instance variables 
+  int size; 
+  String breed; 
+  String name; 
+  
+  // a method 
+  void bark() {
+    System.out.println("Ruff! Ruff!");  
+  }
+}
+//2. Write a tester(TestDrive) class
+class DogTestDrive {
+ // a main method 
+ public static void main (String [] args){
+    // Dog test code goes here
+    Dog d = new Dog(); // Make a Dog object 
+    d.size = 40; // use the dot operator (.) to set the size of the Dog
+    d.bark(); // and to call its bark method. 
+  }
+
+}
